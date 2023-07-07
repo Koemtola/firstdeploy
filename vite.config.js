3b78eef75b637b9256b7140abb755b5b4948d4b4
koemTola <koemtola1216@gmail.com>
2023-07-07 23:42:16 +0700

    uploaded

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ip_cours',
+  base: '/firstdeploy',
   plugins: [vue()],
   resolve: {
     alias: {
