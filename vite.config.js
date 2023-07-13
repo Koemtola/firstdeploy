817adbca089a8d7b97b26b3de2ec0fa60693875f
KOEM_Tola <koemtola1216@gmail.com>
2023-07-13 09:22:40 +0700

    uploaded

--- vite.config.js
+++ vite.config.js
@@ -5,26 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/firstdeploy',
+  base:'/bookshop/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-
 })
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/ip_cours/'
-//     : '/',
-    
-// }
-// vite.config.js
-const vuePlugin = require('@vitejs/plugin-vue')
-
-module.exports = {
-  plugins: [
-    vuePlugin()
-  ]
-}
